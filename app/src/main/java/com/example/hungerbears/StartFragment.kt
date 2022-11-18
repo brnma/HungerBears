a19e2dfc827fda6e4877845d2de12817fc14c806
Brian Mamani Balderrama <62155144+brnma@users.noreply.github.com>
2022-11-17 19:43:44 -0500

    complete UI

--- app/src/main/java/com/example/hungerbears/StartFragment.kt
+++ app/src/main/java/com/example/hungerbears/StartFragment.kt
@@ -7,26 +7,46 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.hungerbears.databinding.FragmentSelectionBinding
+import com.example.hungerbears.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+    private var numberOfUsers: Int = 1;
+    private var numberOfRestaurants: Int = 1;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_start, container, false)
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
 
-        // Start button section
-        val start: Button = view.findViewById(R.id.startButton)
-        start.setOnClickListener() {
+        binding.startButton.setOnClickListener() {
             findNavController().navigate(R.id.action_startFragment_to_bufferFragment)
         }
+        binding.minusButton1.setOnClickListener() {
+            if (numberOfUsers > 1) {
+                numberOfUsers--
+                binding.numUsersVal.text = numberOfUsers.toString()
+            }
+        }
+        binding.plusButton1.setOnClickListener() {
+            numberOfUsers++
+            binding.numUsersVal.text = numberOfUsers.toString()
+        }
+        binding.minusButton2.setOnClickListener() {
+            if (numberOfRestaurants > 1) {
+                numberOfRestaurants--
+                binding.numRestaurantsVal.text = numberOfRestaurants.toString()
+            }
+        }
+        binding.plusButton2.setOnClickListener() {
+            numberOfRestaurants++
+            binding.numRestaurantsVal.text = numberOfRestaurants.toString()
+        }
 
-        return view
+
+        return binding.root
     }
 }
