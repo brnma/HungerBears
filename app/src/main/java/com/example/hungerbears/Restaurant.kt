2051d356201c9c46064cfe77c5e56c13195f69cf
John Tomas <jtomas@vt.edu>
2022-12-02 18:05:24 -0500

    get distance away from restaurant and use radius search

--- app/src/main/java/com/example/hungerbears/Restaurant.kt
+++ app/src/main/java/com/example/hungerbears/Restaurant.kt
@@ -1,18 +1,25 @@
 package com.example.hungerbears
 
+import android.location.Location
+
 
 class Restaurant {
     private var image = ""
     private var distanceAway: String = ""
     private var name: String = ""
     private var rating: Float = 0f
+    private lateinit var userLocation: Location
+    private lateinit var restLocation: Location
 
 
-    fun setItem(name: String, image: String, dist: Float, rating: Float){
+    fun setItem(name: String, image: String, rating: Float, userLocation: Location, restLocation: Location){
         this.name = name
         this.image = image
-        distanceAway = String.format("%.2f", dist) + " miles away"
         this.rating = rating
+        this.userLocation = userLocation
+        this.restLocation = restLocation
+        calcDistance()
+
     }
 
     fun getImage(): String{
@@ -29,5 +36,9 @@ class Restaurant {
 
     fun getRating(): Float{
         return rating
+    }
+
+    fun calcDistance() {
+        distanceAway =  (String.format("%.2f",(userLocation.distanceTo(restLocation)) / 1609.344f))
     }
 }
--- app/src/main/java/com/example/hungerbears/StartFragment.kt
+++ app/src/main/java/com/example/hungerbears/StartFragment.kt
@@ -1,5 +1,7 @@
 package com.example.hungerbears
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +14,8 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.hungerbears.databinding.FragmentStartBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -23,15 +27,40 @@ class StartFragment : Fragment() {
 
     private val viewModel: SharedViewModel by activityViewModels()
 
+    //A connection to the Google Play location API
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentStartBinding.inflate(inflater, container, false)
 
+        //Create a location client
+        fusedLocationClient = activity?.let { LocationServices.getFusedLocationProviderClient(it) }!!
+
+        //get the last location identified. Also, set a listener that updates the
+        //R.id.coordinates text when the location is found (on Success)
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                // Got the last known location. In some rare situations this can be null.
+                if (location != null) {
+                    viewModel.setUserLng(location.longitude)
+                    viewModel.setUserLat(location.latitude)
+                }
+            }
+
+        //This will let the user know when the location was not able to be found.
+        fusedLocationClient.lastLocation
+            .addOnFailureListener {
+                Toast.makeText(context, "Failed to get location", Toast.LENGTH_SHORT).show()
+            }
+
         binding.startButton.setOnClickListener() {
             viewModel.setNumRestaurants(numberOfRestaurants)
             viewModel.setNumUsers(numberOfUsers)
+            viewModel.setRadius(binding.searchDistanceNum.text.toString())
 
             // call google maps api to get restaurants
 //            GoogleMapsService(this.requireContext()).getRestaurants()
@@ -83,8 +112,9 @@ class StartFragment : Fragment() {
         // Instantiate the RequestQueue
         val queue = Volley.newRequestQueue(context)
         val url =
-            "${BASE_URL}location=37.229572,-80.413940&radius=20000&type=restaurant&key=${API_KEY}"
-        //"${BASE_URL}location=${lat},${lng}&radius=${radius}&type=restaurant&key=${API_KEY}"
+//            "${BASE_URL}location=37.229572,-80.413940&radius=20000&type=restaurant&key=${API_KEY}"
+        "${BASE_URL}location=${viewModel.getUserLat()},${viewModel.getUserLng()}&radius=${viewModel.getRadius()}&type=restaurant&key=${API_KEY}"
+//            "${BASE_URL}location=${viewModel.getUserLat()},${viewModel.getUserLng()}&radius=20000&type=restaurant&key=${API_KEY}"
 
         // Request a string response from the provided URL
         val request = JsonObjectRequest(
@@ -110,9 +140,17 @@ class StartFragment : Fragment() {
 //                    viewModel.setLat(lat)
 //                    viewModel.setLng(lng)
 
-                    val distance = viewModel.getDistance()
                     val restaurantObj = Restaurant()
-                    restaurantObj.setItem(name, photoUrl, distance, rating.toFloat())
+
+                    val userLocation: Location = Location("User Location")
+                    userLocation.longitude = viewModel.getUserLng()
+                    userLocation.latitude = viewModel.getUserLat()
+
+                    val restLocation: Location = Location ("Restaurant Location")
+                    restLocation.longitude = lng
+                    restLocation.latitude = lat
+
+                    restaurantObj.setItem(name, photoUrl, rating.toFloat(), userLocation, restLocation)
                     //TODO: add restaurantObj to list in ViewModel
                     viewModel.addRestaurant(restaurantObj)
                 }
