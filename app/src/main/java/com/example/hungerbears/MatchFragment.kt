0b2da185d01e0bdc9a184166befd960c0cecdfcc
John <jtomas@vt.edu>
2022-12-08 15:09:25 -0500

    shake to get another match

--- app/src/main/java/com/example/hungerbears/MatchFragment.kt
+++ app/src/main/java/com/example/hungerbears/MatchFragment.kt
@@ -1,6 +1,11 @@
 package com.example.hungerbears
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,17 +14,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.hungerbears.databinding.FragmentMatchBinding
 import com.example.hungerbears.databinding.FragmentSelectionBinding
+import java.lang.Math.sqrt
+import java.util.*
 
 
 class MatchFragment : Fragment() {
     private val viewModel: SharedViewModel by activityViewModels()
     private var _binding: FragmentMatchBinding? = null
     private val binding get() = _binding!!
+
+
+    // Declaring sensorManager
+    // and acceleration constants
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,7 +70,56 @@ class MatchFragment : Fragment() {
             viewModel.resetValues()
             findNavController().navigate(R.id.action_matchFragment_to_startFragment)
         }
+
+        // Getting the Sensor Manager instance
+        sensorManager = context?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
         return binding.root
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+
+            // Fetching x,y,z values
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            // Getting current accelerations
+            // with the help of fetched x,y,z values
+            currentAcceleration = kotlin.math.sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            // Display a Toast message if
+            // acceleration value is over 12
+            if (acceleration > 20) {
+                Toast.makeText(context, "Random Match!", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_matchFragment_self)
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
     }
 
 }
